255748d0caa46fca0ffb020f314973435f991793
tg1482 <tg1482@stern.nyu.edu>
2022-12-16 23:56:32 -0800

    rewriting popup.js

--- popup.js
+++ popup.js
@@ -6,6 +6,67 @@ const stopButton = document.getElementById("stop-button");
 const voiceSelect = document.getElementById("voice");
 const speedInput = document.getElementById("speed");
 
+// listen for the start button click event
+startButton.addEventListener("click", () => {
+
+    console.log('started!');
+
+    // disable the start button and enable the stop button
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    
+    // get the latest chatgpt response
+    getChatGptResponse().then((text) => {
+        // start speaking the response, one sentence at a time
+        speak(response);
+      }).catch((error) => {
+        // Handle the error
+        console.error(error.message);
+      });
+
+
+    // enable the start button and disable the stop button
+    startButton.disabled = false;
+    stopButton.disabled = true;
+});
+  
+// listen for the stop button click event
+stopButton.addEventListener("click", () => {
+    // stop speaking
+    stop();
+
+    // enable the start button and disable the stop button
+    startButton.disabled = false;
+    stopButton.disabled = true;
+});
+
+// Function to retrieve the latest text from the chatGPT response text box
+function getChatGptResponse() {
+    // Return a new Promise
+    return new Promise((resolve, reject) => {
+      // Get the current tab
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        
+        console.log('sending message to content.js...')
+        
+        // Send a message to content.js in the active tab
+        chrome.tabs.sendMessage(tabs[0].id, { command: 'getChatGptResponse' }, (response) => {
+          
+            console.log('received response...')
+            
+            // Check if the response is valid
+            if (response && response.text) {
+            // Resolve the Promise with the text
+            resolve(response.text);
+            } else {
+            // Reject the Promise with an error
+            reject(new Error('Error getting text from chatGPT response text box'));
+            }
+        });
+      });
+    });
+  }
+  
 // create a new SpeechSynthesisUtterance instance
 const utterance = new SpeechSynthesisUtterance();
 
@@ -54,27 +115,3 @@ const stop = () => {
   synth.cancel();
 };
 
-// listen for the start button click event
-startButton.addEventListener("click", () => {
-
-  console.log('started!');
-  // get the latest chatgpt response
-  const response = getChatGptResponse();
-
-  // start speaking the response, one sentence at a time
-  speak(response);
-
-  // disable the start button and enable the stop button
-  startButton.disabled = true;
-  stopButton.disabled = false;
-});
-
-// listen for the stop button click event
-stopButton.addEventListener("click", () => {
-  // stop speaking
-  stop();
-
-  // enable the start button and disable the stop button
-  startButton.disabled = false;
-  stopButton.disabled = true;
-});
