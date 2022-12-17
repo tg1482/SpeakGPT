5c2b9cce3a8002c92bc9a38a54834d07452937b2
tg1482 <tg1482@stern.nyu.edu>
2022-12-16 23:55:34 -0800

    spacing in background.js

--- background.js
+++ background.js
@@ -5,19 +5,19 @@ chrome.browserAction.onClicked.addListener(tab => {
       // send a message to the popup script with the chatgpt response
       chrome.runtime.sendMessage({ type: "set-chatgpt-response", response: response.response });
     });
-  });
+});
   
-  // listen for messages from the popup script
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+// listen for messages from the popup script
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     // check if the message is from the popup script
     if (sender.tab) {
-      return;
+        return;
     }
-  
+
     // check if the message is a request to set the chatgpt response
     if (request.type === "set-chatgpt-response") {
-      // store the chatgpt response in the background script
-      const response = request.response;
+        // store the chatgpt response in the background script
+        const response = request.response;
     }
-  });
+});
   
