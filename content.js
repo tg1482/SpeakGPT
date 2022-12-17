e54ddfa5d75394802438b0738e9b1b37fc913799
tg1482 <tg1482@stern.nyu.edu>
2022-12-17 00:53:15 -0800

    adding logic to find the latest response

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+console.log('I am loaded...')
+
 // Listen for messages from popup.js
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     // Check if the message is a getChatGptResponse message
@@ -11,26 +13,32 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         // Check if the response exists
         if (response) {
             // Send the response text to popup.js
-            sendResponse({ text: response.textContent });
+            sendResponse({ text: response });
         } else {
             // Send an error to popup.js
-            sendResponse({ error: 'Error getting text from chatGPT response text box' });
+            sendResponse({ error: 'Error sending text from chatGPT response text box' });
         }
     }
-  });
+});
   
-  // function to find the latest chatgpt response on the page
-  const findChatGptResponse = () => {
+// function to find the latest chatgpt response on the page
+const findChatGptResponse = () => {
     // your code to find the chatgpt response on the page
     // you can use DOM manipulation and traversal techniques such as querySelector()
     // and getElementsByTagName() to find the chatgpt response element
     // and then use its textContent property to get the response text
     // you may need to adjust this function depending on the structure and layout of your page
-    
+
     // const responseElement = document.querySelector(".chatgpt-response");
     // return responseElement ? responseElement.textContent : false;
-    const element = document.getElementsByClassName('react-scroll-to-bottom--css-wczmr-79elbk h-full dark:bg-gray-800')[0];
-    const textContent = element.innerText || element.textContent;
+    // const element = document.getElementsByClassName('react-scroll-to-bottom--css-wczmr-79elbk h-full dark:bg-gray-800')[0];
+    const elements = document.querySelectorAll('[class*="444654"]');
 
-    return textContent
-  };
+    console.log(elements);
+
+    last_response = elements[elements.length - 1].textContent
+
+    console.log(last_response);
+
+    return last_response
+};
